95485e1d34ba8be8ccdefe32b13cdf43e1d9852e
ThePi2K <felixpeerpi@gmail.com>
2022-11-28 09:07:13 +0100

    "SUCCEEDED SUCCESSFULLY" added
    Changes at the end of the batch

--- 0/_media/powershell.bat
+++ 0/_media/powershell.bat
@@ -1,3 +1,9 @@
 powershell.exe -Command "Set-ExecutionPolicy Unrestricted"
 powershell.exe .\_media\restore.ps1
 powershell.exe -Command "Set-ExecutionPolicy Restricted"
+timeout 2 > nul
+cls
+
+echo CHERRY OK SUCCEEDED SUCCESSFULLY
+timeout 2 > nul
+cls
--- 0/_media/restore.ps1
+++ 0/_media/restore.ps1
@@ -1,4 +1,6 @@
 vssadmin delete shadows /all /quiet
 Enable-ComputerRestore -Drive "C:\"
+cls
 vssadmin resize shadowstorage /for=C: /on=C: /maxsize=20%
+cls
 Checkpoint-Computer -Description "Cherry OK" -RestorePointType "MODIFY_SETTINGS"
