81f1a6b535eb6fad24fcf1a81fe358a8ac322959
ThePi2K <felixpeerpi@gmail.com>
2022-12-15 14:48:26 +0100

    Update restore.ps1

--- 0/_media/restore.ps1
+++ 0/_media/restore.ps1
@@ -1,8 +1,8 @@
 vssadmin delete shadows /all /quiet
 Enable-ComputerRestore -Drive "C:\"
 cls
-echo CONFIGURING SYSTEM RESTORE
+echo "CONFIGURING SYSTEM RESTORE"
 vssadmin resize shadowstorage /for=C: /on=C: /maxsize=20%
 cls
-echo CONFIGURING SYSTEM RESTORE
+echo "CONFIGURING SYSTEM RESTORE"
 Checkpoint-Computer -Description "Cherry OK" -RestorePointType "MODIFY_SETTINGS"
