6940d088c6afa6cdb9c5232701efd06ba6bd78c3
ThePi2K <felixpeerpi@gmail.com>
2022-12-19 09:41:13 +0100

    Added Office Apps to Desktop

--- 0/Scripts/set_M365_Taskbar.cmd
+++ 0/Scripts/set_M365_Taskbar.cmd
@@ -5,6 +5,11 @@ copy "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Outlook.lnk" "%AppDat
 copy "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Google Chrome.lnk" "%AppData%\Microsoft\Internet Explorer\Quick Launch\User Pinned\TaskBar"
 copy "%APPDATA%\Microsoft\Windows\Start Menu\Programs\File Explorer.lnk" "%AppData%\Microsoft\Internet Explorer\Quick Launch\User Pinned\TaskBar\Explorer.lnk"
 
+copy "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Word.lnk" "%USERPROFILE%\..\Public\Desktop\"
+copy "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Excel.lnk" "%USERPROFILE%\..\Public\Desktop\"
+copy "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\PowerPoint.lnk" "%USERPROFILE%\..\Public\Desktop\"
+copy "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Outlook.lnk" "%USERPROFILE%\..\Public\Desktop\"
+
 reg import ..\_media\TaskbarM365.reg
 taskkill /f /im explorer.exe
 start explorer.exe
