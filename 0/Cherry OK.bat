4a367a5c4775a216d005c775bcc320b699ff1db1
ThePi2K <felixpeerpi@gmail.com>
2022-12-22 15:37:05 +0100

    Version 5.2.1
    Changed Version Number

--- 0/Cherry OK.bat
+++ 0/Cherry OK.bat
@@ -1,6 +1,6 @@
 :: Cherry OK Script
 :: by Felix Peer
-:: Version 5.2.0
+:: Version 5.2.1
 :: Created and tested for Windows 11 22H2
 
 @echo off
@@ -34,7 +34,7 @@ if not exist C:\Users\Public\Documents\CherryOK (
 powershell.exe -Command "Set-ExecutionPolicy Unrestricted"
 powershell.exe .\_media\echoTitle.ps1
 echo.
-echo    Cherry OK - Version 5.2.0
+echo    Cherry OK - Version 5.2.1
 
 :: CHECK WINDOWS VERSION ::
 ::echo CHECK WINDOWS VERSION
