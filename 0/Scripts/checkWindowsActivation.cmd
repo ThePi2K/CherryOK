cbd49e0313e885c16a03bdf430d58414d6df8524
ThePi2K <felixpeerpi@gmail.com>
2023-01-09 10:31:53 +0100

    new Script to Test

---
+++ 0/Scripts/checkWindowsActivation.cmd
@@ -0,0 +1,13 @@
+@echo off
+
+for /f "tokens=3 delims=: " %%a in (
+    'cscript //nologo "%systemroot%\system32\slmgr.vbs" /dli ^| find "License Status:"' 
+) do set "licenseStatus=%%a"
+
+if /i "%licenseStatus%"=="Licensed" (
+  echo LICENSED
+) else (
+  echo NO KEY FOUND
+)
+
+pause
