723cd7047ac412a0daa5076fd747a0fa305d4548
ThePi2K <felixpeerpi@gmail.com>
2023-01-11 16:15:05 +0100

    Admin Batch Edit

--- 0/_media/admin.bat
+++ 0/_media/admin.bat
@@ -1,25 +1,25 @@
 @echo off
-@title User Account Control
+@title Cherry OK - Admin
 
 :: ## Set UAC ##
-cacls.exe "%SystemDrive%\System Volume Information" >nul 2>nul
-if %errorlevel%==0 goto Admin
-if exist "%temp%\getadmin.vbs" del /f /q "%temp%\getadmin.vbs"
-echo Set RequestUAC = CreateObject^("Shell.Application"^)>"%temp%\getadmin.vbs"
-echo RequestUAC.ShellExecute "%~s0","","","runas",1 >>"%temp%\getadmin.vbs"
-echo WScript.Quit >>"%temp%\getadmin.vbs"
-"%temp%\getadmin.vbs" /f
-if exist "%temp%\getadmin.vbs" del /f /q "%temp%\getadmin.vbs"
-exit
+set isAdminDir=C:\Windows\CherryTestAdmin
+mkdir %isAdminDir%
+if not exist %isAdminDir% (
+	set "params=%*"
+	cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
+)
+rmdir %isAdminDir%
 
-:Admin
+:: SET UAC ::
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v "ConsentPromptBehaviorAdmin" /t reg_dword /d 0 /F >nul
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v "EnableLUA" /t reg_dword /d 0 /F >nul
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v "PromptOnSecureDesktop" /t reg_dword /d 0 /F >nul
+
+:: SET EXECUTION POLICY
 powershell.exe -Command "Set-ExecutionPolicy Unrestricted"
 
 :: START UPDATES ::
-powershell.exe .\_media\Store.ps1
+powershell.exe .\Store.ps1
 wuauclt /detectnow /updatenow
 
 exit
