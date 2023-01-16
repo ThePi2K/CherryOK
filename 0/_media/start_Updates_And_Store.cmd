452b83a9ba743634409f4f9a08d887d28a9dcb21
ThePi2K <felixpeerpi@gmail.com>
2023-01-16 12:07:18 +0100

    6.1b
    automatic c ok start if no upd available

--- 0/_media/start_Updates_And_Store.cmd
+++ 0/_media/start_Updates_And_Store.cmd
@@ -11,3 +11,33 @@ cls
 echo SCANNING AND INSTALLING WINDOWS UPDATES...
 powershell -command "Get-WindowsUpdate"
 powershell -command "Install-WindowsUpdate -ForceDownload -ForceInstall -AcceptAll -AutoReboot"
+
+:: CHECKING UPDATES ::
+echo CHECKING UPDATES
+powershell -command "Get-WindowsUpdate" > tmp
+find /c "ComputerName" tmp >nul
+IF %ERRORLEVEL% EQU 0 (
+	del tmp
+	shutdown /r /t 30
+	exit
+)
+del tmp
+ECHO UPDATES OK
+timeout 2 > nul
+cls
+
+:: STARTING CHERRY OK
+wmic LOGICALDISK where driveType=2 get deviceID > C:\tmp 2>&1
+for /f "skip=1" %%b IN ('type C:\tmp') DO (echo %%b & %%b & IF EXIST "Cherry OK.cmd" goto openCmd)
+
+:openCmd
+del C:\tmp >nul 2>&1
+IF EXIST "Cherry OK.cmd" (
+	cd 0
+	cls
+	call "Cherry OK.bat"
+) else (
+	echo USB is missing...
+	pause
+	exit
+)
