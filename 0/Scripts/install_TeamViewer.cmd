2f407f80fe57764251f6c86d4385d2bca74bf2ef
ThePi2K <felixpeerpi@gmail.com>
2023-01-16 09:38:21 +0100

    Change for TeamViewer Script

--- 0/Scripts/install_TeamViewer.cmd
+++ 0/Scripts/install_TeamViewer.cmd
@@ -1,2 +1,4 @@
 if exist "%userprofile%\Desktop\Cherry Hilfe.exe" del "%userprofile%\Desktop\Cherry Hilfe.exe"
+if exist "%userprofile%\Desktop\Cherry Help.exe" del "%userprofile%\Desktop\Cherry Help.exe"
+if exist "%userprofile%\Desktop\Cherry Aiuto.exe" del "%userprofile%\Desktop\Cherry Aiuto.exe"
 winget install --id TeamViewer.TeamViewer --accept-source-agreements --force
