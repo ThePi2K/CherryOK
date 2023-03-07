9266bfbab6cfb5b16766c9f5a92068e3500068ca
ThePi2K <felixpeerpi@gmail.com>
2023-03-07 10:11:29 +0100

    Update $COPY_USB.cmd

--- 1/$COPY_USB.cmd
+++ 1/$COPY_USB.cmd
@@ -1,20 +1,25 @@
 @echo off
 @title Cherry USB
 
+set drive=%CD:~0,3%
+
+echo %drive%
+
+
 C:
 
 pause
 
-rd /s /q %CD:~0,3%HP
-rd /s /q %CD:~0,3%LOST.DIR
-del %CD:~0,3%BOOTEX.LOG
+rd /s /q %drive%HP
+rd /s /q %drive%LOST.DIR
+del %drive%BOOTEX.LOG
 
 pause
 
 
 
-copy "C:\Users\FelixPeer\OneDrive - Cherry Computer\Documents\Cherry\Cherry USB\Cherry OK.cmd" "%CD:~0,3%"
-robocopy "C:\Users\FelixPeer\OneDrive - Cherry Computer\Documents\Cherry\Cherry USB\0" "%CD:~0,3%0" /MIR
-robocopy "C:\Users\FelixPeer\OneDrive - Cherry Computer\Documents\Cherry\Cherry USB\1" "%CD:~0,3%1" /MIR
+copy "C:\Users\FelixPeer\OneDrive - Cherry Computer\Documents\Cherry\Cherry USB\Cherry OK.cmd" "%drive%"
+robocopy "C:\Users\FelixPeer\OneDrive - Cherry Computer\Documents\Cherry\Cherry USB\0" "%drive%0" /MIR
+robocopy "C:\Users\FelixPeer\OneDrive - Cherry Computer\Documents\Cherry\Cherry USB\1" "%drive%1" /MIR
 
 pause
