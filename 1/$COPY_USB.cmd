8c8c0f3c0248f8840156b6050fdba882024f0f3b
ThePi2K <felixpeerpi@gmail.com>
2023-03-07 10:12:51 +0100

    Final Script

--- 1/$COPY_USB.cmd
+++ 1/$COPY_USB.cmd
@@ -5,21 +5,12 @@ set drive=%CD:~0,3%
 
 echo %drive%
 
-
 C:
-
-pause
 
 rd /s /q %drive%HP
 rd /s /q %drive%LOST.DIR
 del %drive%BOOTEX.LOG
 
-pause
-
-
-
 copy "C:\Users\FelixPeer\OneDrive - Cherry Computer\Documents\Cherry\Cherry USB\Cherry OK.cmd" "%drive%"
 robocopy "C:\Users\FelixPeer\OneDrive - Cherry Computer\Documents\Cherry\Cherry USB\0" "%drive%0" /MIR
 robocopy "C:\Users\FelixPeer\OneDrive - Cherry Computer\Documents\Cherry\Cherry USB\1" "%drive%1" /MIR
-
-pause
