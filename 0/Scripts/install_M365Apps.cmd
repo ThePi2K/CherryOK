cb5d90178d9a32ad875ea8b6518b8ea8c02041f8
ThePi2K <felixpeerpi@gmail.com>
2023-03-30 09:43:34 +0200

    edits

--- 0/Scripts/install_M365Apps.cmd
+++ 0/Scripts/install_M365Apps.cmd
@@ -1,5 +1,5 @@
 if not exist "C:\Program Files\Microsoft Office\root\Office16\WINWORD.EXE" winget install --id Microsoft.Office --accept-source-agreements --force
 REG ADD HKEY_CURRENT_USER\SOFTWARE\Policies\Microsoft\Office\16.0\Teams /v PreventFirstLaunchAfterInstall /t REG_DWORD /d 1 /f
 
-set /p drivers="Taskleiste anpassen? Chrome, Explorer und Office? [y|n] "
-if "%drivers%" == "y" set_M365_Taskbar_and_Desktop.cmd
+set /p links="Taskleiste anpassen? Chrome, Explorer und Office? [y|n] "
+if "%links%" == "y" CherryTool_SetM365Links.cmd
