0fda915b34e1ea063eabe9f75d05d066ab3d9e28
ThePi2K <felixpeerpi@gmail.com>
2023-07-27 11:25:59 +0200

    Update restartCherryOK.cmd

--- 0/_media/restartCherryOK.cmd
+++ 0/_media/restartCherryOK.cmd
@@ -1,20 +1,34 @@
 @echo off
 @title Cherry OK
 
-:: STARTING CHERRY OK
-:checkUSB
-wmic LOGICALDISK where driveType=2 get deviceID > C:\tmp 2>&1
-for /f "skip=1" %%b IN ('type C:\tmp') DO (echo %%b & %%b & IF EXIST "Cherry OK.cmd" goto openCmd)
+:: START AS ADMIN ::
+set isAdminDir=C:\Windows\CherryTestAdmin
+mkdir %isAdminDir% > nul
+cls
+if not exist %isAdminDir% (
+	set "params=%*"
+	cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit )
+	exit
+)
+rmdir %isAdminDir%
 
-:openCmd
-del C:\tmp >nul 2>&1
-IF EXIST "Cherry OK.cmd" (
-	cd 0
-	cls
-	call "Cherry OK.bat"
-) else (
-	echo USB is missing...
-	pause
-	cls
-	goto checkUSB
+
+:checkUSB
+
+wmic LOGICALDISK where driveType=2 get deviceID > C:\tmp 2>&1
+for /f "skip=1" %%b IN ('type C:\tmp') DO (
+	echo %%b
+	%%b
+	del C:\tmp >nul 2>&1
+	dir
+	IF EXIST "Cherry OK.cmd" (
+		cd 0
+		cls
+		call "Cherry OK.bat"
+	) else (
+		echo USB is missing...
+		pause
+		cls
+		goto checkUSB
+	)
 )
