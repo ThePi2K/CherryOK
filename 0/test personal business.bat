2af9afe6fe78b19d9ae1376577900fe2fefab7d7
ThePi2K <felixpeerpi@gmail.com>
2023-08-31 16:17:12 +0200

    Create test personal business.bat

---
+++ 0/test personal business.bat
@@ -0,0 +1,60 @@
+:: Cherry OK Script
+:: by Felix Peer
+:: Designed for Windows 11
+:: Full Automated v7
+
+@echo off
+@set version=7 Beta
+@title Cherry OK %version%
+
+
+
+:: PERSONAL OR BUSINESS ::
+choice /C YN /N /M "Personal? ("N" for Business) [Y or N]"
+if errorlevel 2 (
+pause
+
+:: INSTALLING ATERA ::
+    start Programme/AteraAgentUnassigned.msi
+    cls
+
+
+:: RENAMING PC
+choice /C YN /N /M "Do you want to rename the computer? [Y or N]"
+if errorlevel 2 (
+    echo Rename operation canceled.
+    timeout 1 > nul
+    goto skip
+)
+timeout 2 > nul
+cls
+
+:restart
+set /p customerNumber=Enter customer number (5 digits): 
+set /p deviceType=Enter device type: 
+set /p deviceNumber=Enter device number (2 digits): 
+
+for /f "usebackq delims=" %%I in (`powershell "\"%deviceType%\".toUpper()"`) do set "deviceType_upper=%%~I"
+
+set newname=%customerNumber%-%deviceType_upper%-%deviceNumber%
+    
+cls
+choice /C YN /N /M "Is this correct? %computername% -> %newname% [Y or N]"
+if errorlevel 2 (
+    timeout 1 > nul
+    cls
+    goto restart
+)
+timeout 2 > nul
+cls
+
+
+wmic computersystem where name="%computername%" call rename "%newname%"
+cls
+echo Computer renamed to %newname%
+
+)
+timeout 2 > nul
+cls
+
+:skip
