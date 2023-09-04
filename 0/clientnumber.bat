aeb21bd0545fa03a9ca069f8c9e3c996b4fab3ff
ThePi2K <felixpeerpi@gmail.com>
2023-09-04 15:42:53 +0200

    Update clientnumber.bat

--- 0/clientnumber.bat
+++ 0/clientnumber.bat
@@ -4,8 +4,9 @@ set /p Kundennummer="Geben Sie die Kundennummer ein: "
 for /f "tokens=2,3 delims=," %%a in (clienti.csv) do (
     if "%%a" equ "%Kundennummer%" (
         echo Kundennamen: %%b
-        exit /b
+        pause
     )
 )
 
 echo Kundennummer nicht gefunden.
+pause
