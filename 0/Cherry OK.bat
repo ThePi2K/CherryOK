a2b424fb4cfda16902fd4e60e3ad3406259f9758
ThePi2K <felixpeerpi@gmail.com>
2023-09-06 09:05:00 +0200

    moved perso vs business

--- 0/Cherry OK.bat
+++ 0/Cherry OK.bat
@@ -82,90 +82,6 @@ if not exist "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\start_Upda
 )
 
 :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-::                                   PERSONAL OR BUSINESS CHOICE                                   ::
-:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-
-:: PERSONAL OR BUSINESS ::
-:choice
-cls
-echo Choose an option:
-echo 1. Personal
-echo 2. Business
-echo.
-
-set /p c=Your choice: 
-
-if "%c%"=="1" (
-    set "customertype=Personal"
-) else if "%c%"=="2" (
-    set "customertype=Business"
-) else (
-    echo Invalid choice. Please enter 1 or 2.
-    goto choice
-)
-
-:end
-echo You have selected "%customertype%".
-timeout 2 > nul
-cls
-
-if "%customertype%"=="Personal" goto skip
-
-:: INSTALLING ATERA ::
-if not exist "C:\Program Files\ATERA Networks\AteraAgent\AteraAgent.exe" start Programme/AteraAgentUnassigned.msi
-
-:: RENAMING COMPUTER ::
-echo Current name: %computername%
-
-choice /C YN /N /M "Do you want to rename the computer? [Y or N]"
-if errorlevel 2 (
-    echo Rename operation canceled.
-    timeout 1 > nul
-    goto skip
-)
-timeout 2 > nul
-cls
-
-:restart
-set /p customerNumber=Enter customer number (5 digits): 
-set /p deviceType=Enter device type: 
-set /p deviceNumber=Enter device number (2 digits): 
-
-for /f "usebackq delims=" %%I in (`powershell "\"%deviceType%\".toUpper()"`) do set "deviceType_upper=%%~I"
-
-set newname=%customerNumber%-%deviceType_upper%-%deviceNumber%
-
-:: CHECKING CUSTOMER NUMBER ::
-cls
-for /f "tokens=2,3 delims=," %%a in (_media\clienti.csv) do (
-    if "%%a" equ "%customerNumber%" (
-        echo Customer: %%b
-    )
-)
-
-choice /C YN /N /M "Is this correct? %computername% -> %newname% [Y or N]"
-if errorlevel 2 (
-    timeout 1 > nul
-    cls
-    goto restart
-)
-timeout 2 > nul
-cls
-
-wmic computersystem where name="%computername%" call rename "%newname%"
-cls
-echo Computer renamed to %newname%
-timeout 2 > nul
-
-:: END RENAMING COMPUTER ::
-
-:skip
-echo.
-echo Continuing...
-timeout 2 > nul
-cls
-
-:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 ::                                         CHERRY OK START                                         ::
 :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
@@ -260,6 +176,94 @@ IF %ERRORLEVEL% NEQ 0 (
 	shutdown /r /t 0
 	exit
 )
+
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+::                                   PERSONAL OR BUSINESS CHOICE                                   ::
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+:: PERSONAL OR BUSINESS ::
+:choice
+cls
+echo Choose an option:
+echo 1. Personal
+echo 2. Business
+echo.
+
+set /p c=Your choice: 
+
+if "%c%"=="1" (
+    set "customertype=Personal"
+) else if "%c%"=="2" (
+    set "customertype=Business"
+) else (
+    echo Invalid choice. Please enter 1 or 2.
+    goto choice
+)
+
+:end
+echo You have selected "%customertype%".
+timeout 2 > nul
+cls
+
+if "%customertype%"=="Personal" goto skip
+
+:: INSTALLING ATERA ::
+if not exist "C:\Program Files\ATERA Networks\AteraAgent\AteraAgent.exe" start Programme/AteraAgentUnassigned.msi
+
+:: RENAMING COMPUTER ::
+echo Current name: %computername%
+
+choice /C YN /N /M "Do you want to rename the computer? [Y or N]"
+if errorlevel 2 (
+    echo Rename operation canceled.
+    timeout 1 > nul
+    goto skip
+)
+timeout 2 > nul
+cls
+
+:restart
+set /p customerNumber=Enter customer number (5 digits): 
+set /p deviceType=Enter device type: 
+set /p deviceNumber=Enter device number (2 digits): 
+
+for /f "usebackq delims=" %%I in (`powershell "\"%deviceType%\".toUpper()"`) do set "deviceType_upper=%%~I"
+
+set newname=%customerNumber%-%deviceType_upper%-%deviceNumber%
+
+:: CHECKING CUSTOMER NUMBER ::
+cls
+for /f "tokens=2,3 delims=," %%a in (_media\clienti.csv) do (
+    if "%%a" equ "%customerNumber%" (
+        echo Customer: %%b
+    )
+)
+
+choice /C YN /N /M "Is this correct? %computername% -> %newname% [Y or N]"
+if errorlevel 2 (
+    timeout 1 > nul
+    cls
+    goto restart
+)
+timeout 2 > nul
+cls
+
+wmic computersystem where name="%computername%" call rename "%newname%"
+cls
+echo Computer renamed to %newname%
+timeout 2 > nul
+
+:: END RENAMING COMPUTER ::
+
+:skip
+echo.
+echo Continuing...
+timeout 2 > nul
+cls
+
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+::                                            CONTINUING                                           ::
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
 :: SYNC TIME ::
 echo TIME SYNCING STARTING...
