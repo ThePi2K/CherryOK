30e1bd5050ef0b1bf9f699f2d8b49ad0cfa23b89
ThePi2K <felixpeerpi@gmail.com>
2023-09-11 15:07:27 +0200

    Update Install 128 GB Update.cmd

--- 1/Install 128 GB Update.cmd
+++ 1/Install 128 GB Update.cmd
@@ -1,6 +1,7 @@
 @echo off
 echo Installing Update...
-wusa.exe windows11.0-kb5029263-x64_4f5fe19bbec786f5e445d3e71bcdf234fe2cbbec.msu /quiet /norestart
-cls
+for %%f in (windows11.0*.msu) do (
+    wusa.exe "%%f" /norestart
+)
 echo Finished!
 pause
