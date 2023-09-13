268bd12c019750d3b804f8e6d8463e755daa9b2e
ThePi2K <felixpeerpi@gmail.com>
2023-09-13 11:26:43 +0200

    nach set taskbar cls

--- 0/Cherry OK.bat
+++ 0/Cherry OK.bat
@@ -425,11 +425,12 @@ cls
 
 :: SET TASKBAR ::
 echo SET TASKBAR
-timeout 2 > nul
 if exist "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Google Chrome.lnk" copy "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Google Chrome.lnk" "%AppData%\Microsoft\Internet Explorer\Quick Launch\User Pinned\TaskBar" >nul 2>&1
 if exist "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Google Chrome.lnk" copy "C:\Users\User\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Google Chrome.lnk" "%AppData%\Microsoft\Internet Explorer\Quick Launch\User Pinned\TaskBar" >nul 2>&1
 copy "%APPDATA%\Microsoft\Windows\Start Menu\Programs\File Explorer.lnk" "%AppData%\Microsoft\Internet Explorer\Quick Launch\User Pinned\TaskBar\Explorer.lnk" >nul 2>&1
 reg import _media\Taskbar.reg >nul 2>&1
+timeout 2 > nul
+cls
 
 :: CLEAR NOTIFICATIONS ::
 echo CLEAR NOTIFICATIONS
