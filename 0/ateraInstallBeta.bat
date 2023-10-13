cded66e6a8453af2c995d950a71d0a8645e7a686
ThePi2K <felixpeerpi@gmail.com>
2023-10-13 17:09:09 +0200

    added Anführungszeichen

--- 0/ateraInstallBeta.bat
+++ 0/ateraInstallBeta.bat
@@ -33,6 +33,6 @@ if not exist "C:\Program Files\ATERA Networks\AteraAgent\AteraAgent.exe" (
     if not "%customerFound%" equ "1" (
         start Programme/AteraAgent.msi
     ) else (
-        call %curl%
+        call "%curl%"
     )
 )
