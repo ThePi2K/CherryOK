e1b99d19fb0b01de12aa4b9ebd21831f34e6a9ca
ThePi2K <felixpeerpi@gmail.com>
2023-10-13 17:13:32 +0200

    Update ateraInstallBeta.bat

--- 0/ateraInstallBeta.bat
+++ 0/ateraInstallBeta.bat
@@ -33,6 +33,8 @@ if not exist "C:\Program Files\ATERA Networks\AteraAgent\AteraAgent.exe" (
     if not "%customerFound%" equ "1" (
         start Programme/AteraAgent.msi
     ) else (
+	echo "%curl%"
+pause
         call "%curl%"
     )
 )
