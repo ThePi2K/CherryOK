b2ec6e27a1843da6229667c277dee84c1a9e9bea
ThePi2K <felixpeerpi@gmail.com>
2023-10-25 09:26:06 +0200

    new convert script atera

--- 0/_media/cmd.cmd
+++ 0/_media/cmd.cmd
@@ -1,54 +1,33 @@
-:: DANKE AN BING AI
+:: DANKE AN CHAT GPT
 
 @echo off
 setlocal enabledelayedexpansion
 
 set "inputFile=atera.csv"
-set "outputFile=atera_fixed.csv"
+set "outputFile=atera_modified.csv"
 
-REM Remove old file if existing
-del %outputFile% /f > nul
-
-REM Create a new file
-type nul > %outputFile%
-
-REM Initialize a line counter
-set /a "counter=0"
-
-REM Read the file line by line
-for /f "delims=" %%a in (%inputFile%) do (
-    set "line=%%a"
-
-    REM Increase the counter
-    set /a "counter+=1"
-    
-    REM Replace semicolons with commas
-    set "line=!line:;=,!"
-
-    REM If it's the first line, just write it to the new file
-    if !counter! equ 1 (
-        echo !line! >> %outputFile%
-        goto nextline
-    )
-
-    REM Remove the first and last quote in the last column
-    for /f "tokens=1-4 delims=," %%i in ("!line!") do (
-        set "part1=%%i"
-        set "part2=%%j"
-        set "part3=%%k"
-        set "part4=%%l"
-        
-        REM Remove the first quote
-        set "part4=!part4:~1!"
-        
-        REM Remove the last quote
-        set "part4=!part4:~0,-1!"
-        
-        REM Combine the parts and write to the new file
-        echo !part1!,!part2!,!part3!,!part4! >> %outputFile%
-    )
-
-    :nextline
+if not exist %inputFile% (
+    echo Die Datei %inputFile% existiert nicht.
+    exit /b 1
 )
 
-endlocal
+del %outputFile%
+
+for /f "usebackq tokens=1-4 delims=;" %%a in ("%inputFile%") do (
+    set "col1=%%a"
+    set "col2=%%b"
+    set "col3=%%c"
+    set "col4=%%d"
+
+    if not defined header (
+        echo !col1!,!col2!,!col3!,!col4! > %outputFile%
+        set "header=true"
+    ) else (
+        set "col4=!col4:~1,-1!"
+        echo !col1!,!col2!,!col3!,!col4! >> %outputFile%
+    )
+)
+
+echo Die Datei %outputFile% wurde erfolgreich erstellt.
+
+pause
