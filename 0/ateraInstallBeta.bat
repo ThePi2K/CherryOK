0c49c51fbfd3dc2fce56902ce37bc22a7176f688
ThePi2K <felixpeerpi@gmail.com>
2023-10-25 15:52:36 +0200

    test

--- 0/ateraInstallBeta.bat
+++ 0/ateraInstallBeta.bat
@@ -51,10 +51,7 @@ if not exist "C:\Program Files\ATERA Networks\AteraAgent\AteraAgent.exe" (
     ) else (
         echo Installing Atera Agent for %customerName%...
         timeout /t 3 > nul
-        echo !curl! > installAtera%customerNumber%.cmd
-        echo exit >> installAtera%customerNumber%.cmd
-        installAtera%customerNumber%.cmd
-        del installAtera%customerNumber%.cmd /f > nul
+        call !curl!
 	)
 ) else echo Atera is already installed!
 
