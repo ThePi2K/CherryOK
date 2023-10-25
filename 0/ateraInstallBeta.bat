ec2025b229fb072279669efb5d6ac0c4c6e49e2a
ThePi2K <felixpeerpi@gmail.com>
2023-10-25 09:42:38 +0200

    final Beta Edition of the atera script

--- 0/ateraInstallBeta.bat
+++ 0/ateraInstallBeta.bat
@@ -1,8 +1,13 @@
 @echo off
 setlocal enabledelayedexpansion
 
-::set /p customerNumber=Enter customer number (5 digits): 
-set customerNumber=00069
+:restartcustomer
+color CF
+echo Loading Customer file...
+forfiles /P "_media" /M "clienti.csv" /C "cmd /c echo The file was last modified on @fdate"
+timeout 2 > nul
+echo.
+set /p customerNumber=Enter the customer number (5 digits): 
 
 :: CHECKING CUSTOMER NUMBER ::
 cls
@@ -12,6 +17,19 @@ for /f "tokens=2,3 delims=," %%a in (_media\clienti.csv) do (
     )
 )
 echo Customer: %customerName%
+
+choice /C YN /N /M "Is this correct? [Y or N]"
+if errorlevel 2 (
+    timeout 1 > nul
+    cls
+    goto restartcustomer
+)
+timeout 2 > nul
+cls
+
+
+
+
 
 :: CHECKING CUSTOMER IN ATERA ::
 set customerFound=0
@@ -29,11 +47,11 @@ if not exist "C:\Program Files\ATERA Networks\AteraAgent\AteraAgent.exe" (
     if not "%customerFound%" equ "1" (
         echo Installing unassigned Atera Agent...
         timeout /t 3 > nul
-        echo start Programme/AteraAgent.msi
+        start Programme/AteraAgent.msi
     ) else (
         echo Installing Atera Agent for %customerName%...
         timeout /t 3 > nul
-        echo call !curl!
+        call !curl!
 	)
 )
 
