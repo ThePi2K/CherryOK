a7deef6c7804cd25cff690c2c74020545c7bcf90
ThePi2K <felixpeerpi@gmail.com>
2023-10-27 10:22:21 +0200

    last beta for atera, i think we did it!

--- 0/ateraInstallBeta.bat
+++ 0/ateraInstallBeta.bat
@@ -7,7 +7,8 @@ echo Loading Customer file...
 forfiles /P "_media" /M "clienti.csv" /C "cmd /c echo The file was last modified on @fdate"
 timeout 2 > nul
 echo.
-set /p customerNumber=Enter the customer number (5 digits): 
+::set /p customerNumber=Enter the customer number (5 digits): 
+set customerNumber=00069
 
 :: CHECKING CUSTOMER NUMBER ::
 cls
@@ -18,14 +19,9 @@ for /f "tokens=2,3 delims=," %%a in (_media\clienti.csv) do (
 )
 echo Customer: %customerName%
 
-choice /C YN /N /M "Is this correct? [Y or N]"
-if errorlevel 2 (
-    timeout 1 > nul
-    cls
-    goto restartcustomer
-)
-timeout 2 > nul
-cls
+
+
+pause
 
 
 
@@ -35,7 +31,7 @@ cls
 set customerFound=0
 for /f "tokens=2,3,4 delims=," %%a in (_media\atera_modified.csv) do (
     if "%%a" equ "%customerNumber%" (
-        set curl=%%c
+        set "curl=%%c"
         set customerFound=1
     )
 )
@@ -46,12 +42,13 @@ if not exist "C:\Program Files\ATERA Networks\AteraAgent\AteraAgent.exe" (
     echo Atera does not exist [only Beta]
     if not "%customerFound%" equ "1" (
         echo Installing unassigned Atera Agent...
-        timeout /t 3 > nul
+        timeout /t 1 > nul
         start Programme/AteraAgent.msi
     ) else (
         echo Installing Atera Agent for %customerName%...
-        timeout /t 3 > nul
-        call !curl!
+        timeout /t 1 > nul
+        cmd /c "!curl!" && del setup.msi
+        )
 	)
 ) else echo Atera is already installed!
 
