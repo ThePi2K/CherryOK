9587ac5e5613dac63db49eba6d7a40b198b5c154
ThePi2K <felixpeerpi@gmail.com>
2023-12-06 17:19:14 +0100

    fixed broken design script

--- 0/Beta Test new Design/restoredesign.cmd
+++ 0/Beta Test new Design/restoredesign.cmd
@@ -1,15 +1,7 @@
 @echo off
 set "Zielordner=%LOCALAPPDATA%\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState"
 
+REM Löschen der Dateien
 del "%Zielordner%\cp2077L.gif"
-
-REM Zurückkopieren der alten settings.json aus der Sicherung settings.old, falls vorhanden
-if exist "%Zielordner%\settings.old" (
-    copy "%Zielordner%\settings.old" "%Zielordner%\settings.json"
-    echo Die alte settings.json wurde erfolgreich wiederhergestellt.
-) else (
-    echo Die Sicherung settings.old existiert nicht. Es gibt keine vorherige Version zum Wiederherstellen.
-)
-
-echo Prozess abgeschlossen.
-pause
+copy %Zielordner%\settings.old %Zielordner%\settings.json
+del "%Zielordner%\settings.old"
--- 0/Beta Test new Design/testdesign.cmd
+++ 0/Beta Test new Design/testdesign.cmd
@@ -4,25 +4,11 @@
 @echo off
 set "Zielordner=%LOCALAPPDATA%\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState"
 
+REM Schriftart kopieren
+copy "PxPlus_IBM_VGA8.ttf" "%WINDIR%\Fonts"
+reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Fonts" /v "PxPlus IBM VGA8 (OpenType)" /t REG_SZ /d PxPlus_IBM_VGA8.ttf /f
+
 REM Kopieren der Dateien
 copy "cp2077L.gif" "%Zielordner%"
-
-REM Inhalte der settings.json sichern und zusammenführen
-set "Quellsettings=settings.json"
-set "Zielsettings=%Zielordner%\settings.json"
-set "ZielsettingsOld=%Zielordner%\settings.old"
-
-REM Überprüfen, ob die Quell- und Zieldateien vorhanden sind, bevor sie bearbeitet werden
-if exist "%Quellsettings%" (
-    if exist "%Zielsettings%" (
-        copy "%Zielsettings%" "%ZielsettingsOld%" > nul
-        type "%Quellsettings%" >> "%Zielsettings%"
-    ) else (
-        copy "%Quellsettings%" "%Zielsettings%"
-    )
-) else (
-    echo Die Quelldatei settings.json wurde nicht im aktuellen Ordner gefunden.
-)
-
-echo Prozess abgeschlossen.
-pause
+copy %Zielordner%\settings.json %Zielordner%\settings.old
+copy settings.json %Zielordner%
