14e5bdb9e632c73f2b223fa10998506b8adecf12
ThePi2K <felixpeerpi@gmail.com>
2023-12-07 11:50:36 +0100

    Create install_Atera_with_Assignement.cmd

---
+++ 0/Scripts/install_Atera_with_Assignement.cmd
@@ -0,0 +1,48 @@
+@echo off
+chcp 65001 > nul
+setlocal enabledelayedexpansion
+
+cd ..
+
+:: CHECKING CUSTOMER NUMBER ::
+cls
+for /f "tokens=2,3 delims=," %%a in (_media\clienti.csv) do (
+    if "%%a" equ "%customerNumber%" (
+        set customerName=%%b
+    )
+)
+echo Customer: %customerName%
+
+choice /C YN /N /M "Is this correct? [Y or N]"
+if errorlevel 2 (
+    timeout 1 > nul
+    cls
+    goto restartcustomer
+)
+timeout 2 > nul
+cls
+
+:: CHECKING CUSTOMER IN ATERA ::
+set customerFound=0
+for /f "tokens=2,3,4 delims=," %%a in (_media\atera_modified.csv) do (
+    if "%%a" equ "%customerNumber%" (
+        set "curl=%%c"
+        set customerFound=1
+    )
+)
+
+:: INSTALLING ATERA ::
+if not exist "C:\Program Files\ATERA Networks\AteraAgent\AteraAgent.exe" (
+    if not "%customerFound%" equ "1" (
+        echo Installing unassigned Atera Agent...
+        timeout /t 1 > nul
+        start Programme/AteraAgent.msi
+    ) else (
+        echo Installing Atera Agent for %customerName%...
+        timeout /t 1 > nul
+        cmd /c "!curl!" && del setup.msi
+        )
+	)
+) else echo Atera is already installed!
+timeout 2 > nul
+cls
