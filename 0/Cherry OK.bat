2577e9c6f26450304c3561826733f4cadb6911fb
ThePi2K <felixpeerpi@gmail.com>
2023-12-21 17:58:25 +0100

    removed skipcherryhelp

--- 0/Cherry OK.bat
+++ 0/Cherry OK.bat
@@ -4,7 +4,7 @@
 :: Semi Automated v7
 
 @echo off
-set version=7.4.1
+set version=7.4.1.1
 title Cherry OK
 chcp 65001 > nul
 setlocal enabledelayedexpansion
@@ -351,7 +351,7 @@ _media\nircmd cmdwait 1500 sendkeypress alt+tab
 del /f "%userprofile%\Desktop\Cherry Hilfe.exe" >nul 2>&1
 del /f "%userprofile%\Desktop\Cherry Aiuto.exe" >nul 2>&1
 del /f "%userprofile%\Desktop\Cherry Help.exe" >nul 2>&1
-if "%customertype%"=="Business" goto skipcherryhelp
+:: if "%customertype%"=="Business" goto skipcherryhelp
 
 echo SETTING UP CHERRY HELP...
 timeout 1 > nul
@@ -381,7 +381,7 @@ if not exist "%userprofile%\Desktop\Cherry *.exe" (
 timeout 1 > nul
 cls
 
-:skipcherryhelp
+:: :skipcherryhelp
 
 :: INSTALL GOOGLE CHROME ::
 if not exist "C:\Program Files\Google\Chrome\Application\chrome.exe" (
