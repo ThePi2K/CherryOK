49e10fb1c740c68c7aa6372fef3d67ad9e657b48
Felix Peer | Cherry Computer <felixpeerpi@gmail.com>
2024-02-29 08:40:19 +0100

    added reg delete for existing reg

--- 0/Cherry OK.bat
+++ 0/Cherry OK.bat
@@ -593,6 +593,8 @@ cls
 reg add "HKLM\SOFTWARE\CherryComputer" /v CherryOKVersion /t REG_SZ /d %version% /f >nul
 reg add "HKLM\SOFTWARE\CherryComputer" /v CherryOKDate /t REG_SZ /d %date% /f >nul
 reg add "HKLM\SOFTWARE\CherryComputer" /v CustomerType /t REG_SZ /d %customertype% /f >nul
+reg delete "HKLM\SOFTWARE\CherryComputer" /v CustomerName /f >nul 2>&1
+reg delete "HKLM\SOFTWARE\CherryComputer" /v customerNumber /f >nul 2>&1
 if "%customertype%"=="Business" reg add "HKLM\SOFTWARE\CherryComputer" /v CustomerName /t REG_SZ /d !customerName! /f >nul
 if "%customertype%"=="Business" reg add "HKLM\SOFTWARE\CherryComputer" /v customerNumber /t REG_SZ /d %customerNumber% /f >nul
 
